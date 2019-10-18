231a82ab0c761d1cf74978d8769a1157df165ae0
firedog7881 <bmeyer24@gmail.com>
2019-10-18 18:49:10 -0400

    initial commit

--- enSiloEventToFIle/Scripts/activate.bat
+++ enSiloEventToFIle/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\bmeye\OneDrive\SourceCode\Projects\enSiloEventToFile\enSiloEventToFIle"
+set "VIRTUAL_ENV=C:\Users\bmeye\OneDrive\SourceCode\Projects\enSiloEventToFile\enSiloEventToFile"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
@@ -23,7 +23,7 @@ if defined _OLD_VIRTUAL_PYTHONHOME (
 )
 
 set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
-set "PROMPT=(enSiloEventToFIle) %PROMPT%"
+set "PROMPT=(enSiloEventToFile) %PROMPT%"
 
 if defined PYTHONHOME (
     set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
